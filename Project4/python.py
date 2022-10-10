1799b1559b3a0a8f5d5639dce094add2cfc4dbb1
OmarElsayd <94146702+OmarElsayd@users.noreply.github.com>
2022-10-09 22:24:24 -0700

    Delete python.py

--- Project4/python.py
+++
@@ -1,8 +0,0 @@
-def convertRGP(red, blue, green):
-    red = 1 - (red / 255) 
-    blue = 1 - (blue / 255) 
-    green = 1 - (green / 255) 
-    return red, blue, green
-
-
-print(convertRGP(139,69,19))
